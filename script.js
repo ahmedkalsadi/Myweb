dd7c4f856c119ede6241f908f17c5473af6d733c
ahmedkalsadi <101634576+ahmedkalsadi@users.noreply.github.com>
2022-03-20 01:39:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,9 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     });
 })
